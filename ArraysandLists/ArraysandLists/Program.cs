cf201066d6097d3addb0b3cce81d3deaa4a88d6c
Jon Frederick <jonFred151@gmail.com>
2023-11-29 19:45:28 -0600

    Exercise 2 finish'

--- ArraysandLists/ArraysandLists/Program.cs
+++ ArraysandLists/ArraysandLists/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace ArraysandLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //establish the array and lists
+           var myNums = new int[10]  {  1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var odds = new List<int>();
+            var even = new List<int>();
+
+
+            //sort the array numbers into evens or odds 
+            for (int i = 0; i < myNums.Length; i++)
+            {
+                if (myNums[i] % 2 == 0)
+                {
+                    even.Add(myNums[i]);
+
+                }
+                else
+                {
+                    odds.Add(myNums[i]);
+                }
+               
+               
+            }
+
+            // writes out the odds number in the list 
+             foreach (var i in odds)
+                { 
+                   Console.WriteLine( $"{i} is an odd number");
+                }
+
+             // write out the even numbers in the list 
+             foreach(var i in even)
+            {
+                Console.WriteLine($"{i} is an even number");
+            }
+
+
+
+
+
+
+        }
+    }
+}
--- ArraysandLists/ArraysandLists/obj/Debug/net8.0/ArraysandLists.AssemblyInfo.cs
+++ ArraysandLists/ArraysandLists/obj/Debug/net8.0/ArraysandLists.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArraysandLists")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8acd6f2730ef7d9f4493e9fa9a5e3700354b2a55")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArraysandLists")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArraysandLists")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
